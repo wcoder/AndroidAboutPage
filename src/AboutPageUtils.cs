0fddfbfbcb351dd3e4da6facaa8d370edf0b77a7
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-06-14 00:19:05 +0300

    Ported utils

--- src/AboutPageUtils.cs
+++ src/AboutPageUtils.cs
@@ -1,11 +1,13 @@
 using Android.Content;
 using Android.Content.PM;
+using Android.OS;
+using Android.Util;
 
 namespace AndroidAboutPage
 {
 	/**
 	 * Created original by medyo on 3/25/16.
-	 * Ported to Xamarin by Yauheni Pakala on 5/1/16.
+	 * Ported to Xamarin by Yauheni Pakala on 6/14/17.
 	 */
 	public static class AboutPageUtils
 	{
@@ -23,6 +25,22 @@ namespace AndroidAboutPage
 				installed = false;
 			}
 			return installed;
+		}
+		public static int GetThemeAccentColor(Context context)
+		{
+			int colorAttr;
+			if (Build.VERSION.SdkInt >= Build.VERSION_CODES.Lollipop)
+			{
+				colorAttr = Android.Resource.Attribute.ColorAccent;
+			}
+			else
+			{
+				//Get colorAccent defined for AppCompat
+				colorAttr = context.Resources.GetIdentifier("colorAccent", "attr", context.PackageName);
+			}
+			var outValue = new TypedValue();
+			context.Theme.ResolveAttribute(colorAttr, outValue, true);
+			return outValue.Data;
 		}
 	}
 }
