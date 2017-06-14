4d9c0a4e9c1abf0479cb5656e06fa1a7a90b8b92
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-06-14 11:44:15 +0300

    Update sample project

--- samples/MainActivity.cs
+++ samples/MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
+using Android.Content.Res;
+using Android.Icu.Util;
 using Android.Views;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Widget;
 using AndroidAboutPage;
 
 namespace AboutPageSamples
@@ -19,26 +22,69 @@ namespace AboutPageSamples
 		{
 			base.OnCreate(bundle);
 
-			Element versionElement = new Element { Title = "Version 6.2" };
-			Element adsElement = new Element { Title = "Advertise with us" };
+			SimulateDayNight(/* DAY */ 2);
 
-			View aboutPage = new AboutPage(this)
-				.IsRTL(false)
+			var versionElement = new Element {Title = "Version 6.2" };
+			var adsElement = new Element { Title = "Advertise with us" };
+
+			var aboutPage = new AboutPage(this)
+				.IsRtl(false)
 				.SetImage(Resource.Drawable.dummy_image)
 				.AddItem(versionElement)
 				.AddItem(adsElement)
 				.AddGroup("Connect with us")
 				.AddEmail("elmehdi.sakout@gmail.com")
-				.AddWebsite("http://medyo.github.io/")
+				.AddWebsite("http://wcoder.github.io/")
 				.AddFacebook("the.medy")
-				.AddTwitter("medyo80")
+				.AddTwitter("evgeniypakalo")
 				.AddYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
 				.AddPlayStore("com.ideashower.readitlater.pro")
-				.AddGitHub("medyo")
-				.AddInstagram("medyo80")
+				.AddGitHub("wcoder")
+				.AddInstagram("evgeniypakalo")
+				.AddItem(GetCopyRightsElement())
 				.Create();
 
 			SetContentView(aboutPage);
+		}
+
+		private Element GetCopyRightsElement()
+		{
+			var copyrights = string.Format(GetString(Resource.String.copy_right), Calendar.Instance.Get(CalendarField.Year));
+
+			return new Element
+			{
+				Title = copyrights,
+				IconDrawable = Resource.Drawable.about_icon_copy_right,
+				IconTint = Resource.Color.about_item_icon_color,
+				IconNightTint = Android.Resource.Color.White,
+				Gravity = GravityFlags.Center,
+				ClickHandler = (sender, args) =>
+				{
+					Toast.MakeText(this, copyrights, ToastLength.Short).Show();
+				}
+			};
+		}
+
+		private void SimulateDayNight(int currentSetting)
+		{
+			const int day = 0;
+			const int night = 1;
+			const int followSystem = 3;
+
+			var currentNightMode = Resources.Configuration.UiMode & UiMode.NightMask;
+
+			if (currentSetting == day && currentNightMode != UiMode.NightNo)
+			{
+				AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightNo;
+			}
+			else if (currentSetting == night && currentNightMode != UiMode.NightYes)
+			{
+				AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightYes;
+			}
+			else if (currentSetting == followSystem)
+			{
+				AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightFollowSystem;
+			}
 		}
 	}
 }
